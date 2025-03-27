833359dcf9f284292c5fcd0f0ca92501f22a4207
Gurusamy-zoho <gurusamysivasankar591@gmail.com>
2025-03-27 20:37:21 +0530

    feat: add swalAlert

--- script.js
+++ script.js
@@ -14,9 +14,24 @@ function calculate() {
     try {
         inputBox.value = eval(inputBox.value.replace('x', '*'));  
     } catch (error) {
-        alert("Invalid Expression"); 
+        Swal.fire({
+            title: 'Invalid Expression',
+            html: `Given correct value`,
+            icon: 'error',
+            confirmButtonText: `Ok`,
+            position: 'center',
+            width: '500px',
+            heightAuto: false,
+            showClass: {
+                popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+                popup: 'animate__animated animate__fadeOutUp'
+            }
+        });
     }
-}
+    }
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
